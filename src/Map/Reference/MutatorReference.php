3503bfe106b9a65d3632f0b7b3155879e2c07885
Estel Smith <estel.smith@gmail.com>
2015-09-07 18:14:11 -0400

    Added Mutator and Property references.

---
+++ src/Map/Reference/MutatorReference.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Cascade\Mapper\Map\Reference;
+
+use Doctrine\Common\Inflector\Inflector;
+
+class MutatorReference implements ReferenceInterface
+{
+    /**
+     * @var \ReflectionClass
+     */
+    private $class;
+
+    /**
+     * @var \ReflectionMethod
+     */
+    private $getter;
+
+    /**
+     * @var \ReflectionMethod
+     */
+    private $setter;
+
+    /**
+     * @param string $class
+     * @param string $field
+     * @param null|string $getterMethod
+     * @param null|string $setterMethod
+     */
+    public function __construct($class, $field, $getterMethod = null, $setterMethod = null)
+    {
+        $this->class = $reflectedClass = new \ReflectionClass($class);
+
+        $getter = $getterMethod ?: 'get' . Inflector::classify($field);
+        $setter = $setterMethod ?: 'set' . Inflector::classify($field);
+
+        $this->getter = $reflectedClass->getMethod($getter);
+        $this->setter = $reflectedClass->getMethod($setter);
+    }
+
+    public function getValue($instance)
+    {
+        $class = $this->class;
+        $getter = $this->getter;
+
+        $className = $class->getName();
+
+        if (!is_subclass_of($instance, $className)) {
+            // @TODO: Throw invalid instance exception.
+        }
+
+        return $getter->invoke($instance);
+    }
+
+    public function setValue($instance, $value)
+    {
+        $class = $this->class;
+        $setter = $this->setter;
+
+        $className = $class->getName();
+
+        if (!is_subclass_of($instance, $className)) {
+            // @TODO: Throw invalid instance exception.
+        }
+
+        $setter->invoke($instance, $value);
+
+        return $instance;
+    }
+}
---
+++ src/Map/Reference/PropertyReference.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Cascade\Mapper\Map\Reference;
+
+class PropertyReference implements ReferenceInterface
+{
+    /**
+     * @var \ReflectionClass
+     */
+    private $class;
+
+    /**
+     * @var string
+     */
+    private $property;
+
+    /**
+     * @param string $class
+     * @param string $property
+     */
+    public function __construct($class, $property)
+    {
+        if (!class_exists($class)) {
+            // @TODO: Throw missing class exception.
+        }
+
+        $this->class = $reflectedClass = new \ReflectionClass($class);
+
+        if ($reflectedClass->getName() !== 'stdClass') {
+            if (!$reflectedClass->hasProperty($property)) {
+                // @TODO: Throw missing property exception.
+            }
+        }
+
+        $this->property = $property;
+    }
+
+    public function getValue($instance)
+    {
+        $class = $this->class;
+        $property = $this->property;
+
+        $className = $class->getName();
+
+        if (!is_subclass_of($instance, $className)) {
+            // @TODO: Throw invalid instance exception.
+        }
+
+        if ($className === 'stdClass') {
+            return $instance->$property;
+        }
+
+        return $class->getProperty($property)->getValue($instance);
+    }
+
+    public function setValue($instance, $value)
+    {
+        $class = $this->class;
+        $property = $this->property;
+
+        $className = $class->getName();
+
+        if (!is_subclass_of($instance, $className)) {
+            // @TODO: Throw invalid instance exception.
+        }
+
+        if ($className === 'stdClass') {
+            $instance->$property = $value;
+        } else {
+            $class->getProperty($property)->setValue($instance, $value);
+        }
+
+        return $instance;
+    }
+}
