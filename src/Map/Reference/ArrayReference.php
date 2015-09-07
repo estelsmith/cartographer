a80d5eff00f7851d41da85d81b272480a6924587
Estel Smith <estel.smith@gmail.com>
2015-09-07 18:22:27 -0400

    Added the ability to reference arrays.

---
+++ src/Map/Reference/ArrayReference.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Cascade\Mapper\Map\Reference;
+
+class ArrayReference implements ReferenceInterface
+{
+    /**
+     * @var string
+     */
+    private $field;
+
+    /**
+     * @param string $field
+     */
+    public function __construct($field)
+    {
+        $this->field = $field;
+    }
+
+    public function getValue($instance)
+    {
+        $field = $this->field;
+
+        if (array_key_exists($field, $instance)) {
+            return $instance[$field];
+        }
+
+        return null;
+    }
+
+    public function setValue($instance, $value)
+    {
+        $instance[$this->field] = $value;
+
+        return $instance;
+    }
+}
