f97c4154d586f23c16f8619a47f27c65870a0e23
Estel Smith <estel.smith@gmail.com>
2015-09-08 09:51:08 -0400

    Added a fluent interface to Map to allow easy buildout of mappings.

--- src/Map.php
+++ src/Map.php
@@ -2,35 +2,126 @@
 
 namespace Cascade\Mapper;
 
+use Cascade\Mapper\Map\Mapping;
 use Cascade\Mapper\Map\MappingInterface;
+use Cascade\Mapper\Map\Reference\ArrayReference;
+use Cascade\Mapper\Map\Reference\MutatorReference;
+use Cascade\Mapper\Map\Reference\PropertyReference;
+use Cascade\Mapper\Map\Reference\ReferenceInterface;
 
-/**
- * @TODO: Add fluent interface for building mappings in favor of passing mappings into the constructor.
- */
 class Map implements MapInterface
 {
+    const REF_ARRAY = 'ref_array';
+    const REF_CLASS_PROPERTIES = 'ref_class_properties';
+    const REF_CLASS_MUTATORS = 'ref_class_mutators';
+
+    /**
+     * @var string
+     */
+    private $from = self::REF_ARRAY;
+
     /**
      * @var MappingInterface[]
      */
     private $mappings = [];
 
     /**
-     * @param MappingInterface[] $mappings
+     * @var string
      */
-    public function __construct($mappings)
-    {
-        foreach ($mappings as $mapping) {
-            $this->addMapping($mapping);
-        }
-    }
+    private $to = self::REF_ARRAY;
 
     public function getMappings()
     {
         return $this->mappings;
     }
 
-    private function addMapping(MappingInterface $mapping)
+    /**
+     * @param string $from
+     * @return $this
+     */
+    public function from($from)
+    {
+        if ($this->isValidRefType($from)) {
+            $this->from = $from;
+            return $this;
+        }
+
+        // @TODO: Throw Invalid From exception.
+        return $this;
+    }
+
+    /**
+     * @param string $to
+     * @return $this
+     */
+    public function to($to)
+    {
+        if ($this->isValidRefType($to)) {
+            $this->to = $to;
+            return $this;
+        }
+
+        // @TODO: Throw Invalid To exception.
+        return $this;
+    }
+
+    /**
+     * @param string $fromField
+     * @param string $toField
+     * @return $this
+     */
+    public function add($fromField, $toField)
+    {
+        $destinationReference = $this->referenceField($this->to, $toField);
+        $sourceReference = $this->referenceField($this->from, $fromField);
+
+        $this->mappings[] = new Mapping($destinationReference, $sourceReference);
+
+        return $this;
+    }
+
+    /**
+     * @param MappingInterface $mapping
+     * @return $this
+     */
+    public function addMapping(MappingInterface $mapping)
     {
         $this->mappings[] = $mapping;
+        return $this;
+    }
+
+    /**
+     * @param string $refType
+     * @return bool
+     */
+    private function isValidRefType($refType)
+    {
+        $validRefTypes = [self::REF_ARRAY, self::REF_CLASS_PROPERTIES, self::REF_CLASS_MUTATORS];
+
+        if (in_array($refType, $validRefTypes, true)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param string $refType
+     * @param string $field
+     * @return ReferenceInterface
+     */
+    private function referenceField($refType, $field)
+    {
+        switch ($refType) {
+            case self::REF_CLASS_PROPERTIES:
+                return new PropertyReference($field);
+                break;
+            case self::REF_CLASS_MUTATORS:
+                return new MutatorReference($field);
+                break;
+            default:
+                return new ArrayReference($field);
+                break;
+        }
     }
 }
